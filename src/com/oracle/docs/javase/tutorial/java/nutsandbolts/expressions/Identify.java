10123c0f1ffe5f941a0ea1988c896ec1088990aa
Gregory Langlais <general@gregorylanglais.com>
2014-03-17 19:48:33 -0400

    Expressions

---
+++ src/com/oracle/docs/javase/tutorial/java/nutsandbolts/expressions/Identify.java
@@ -0,0 +1,13 @@
+package com.oracle.docs.javase.tutorial.java.nutsandbolts.expressions;
+
+/**
+ * Identify Expression Statements
+ */
+public class Identify {
+  public static void main(String[] args) {
+    double aValue = 8933.234;  // assignment statement
+    aValue++;  // increment statement
+    System.out.println("Hello World");  // method invocation statement
+    Object myBike = new Object(); // object creation statement
+  }
+}
